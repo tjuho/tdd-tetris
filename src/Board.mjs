9e1c6f36e677d700a7dc2c9727bcbbfac81d2ba7
Juho <krooh@hotmail.com>
2022-05-24 10:34:45 +0300

    starts with an empty board

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,29 @@
 export class Board {
   width;
   height;
+  matrix;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.clearBoard()
+  }
+
+  clearBoard() {
+    this.matrix = []
+    for (let r=0; r<this.height; r++){
+      this.matrix.push(Array(this.width).fill(false))
+    }
   }
 
   toString() {
-    return "TODO";
+    let result = ''
+    for (let r=0; r<this.height; r++){
+      for (let c=0; c<this.width; c++){
+        result += (this.matrix[r][c]) ? 'X':'.';
+      }
+      result += '\n'
+    }
+    return result;
   }
 }
