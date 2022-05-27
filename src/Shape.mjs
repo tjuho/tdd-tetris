8cc3b004f7759fad9710b6fa0a03cf18fd764059
Juho <krooh@hotmail.com>
2022-05-27 11:42:18 +0300

    Creating a shape base class to all shapes

---
+++ src/Shape.mjs
@@ -0,0 +1,12 @@
+export class Shape{
+    topLeftX
+    topLeftY
+    constructor(topLeftX){
+        this.topLeftX = topLeftX
+        this.topLeftY = 0;
+    }
+    rotateRight();
+    rotateLeft();
+    moveRight();
+    moveLeft();
+}
