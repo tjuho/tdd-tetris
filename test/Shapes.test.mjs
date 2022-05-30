2952e573ba264280428444c90a3914b3bdaac93d
Juho <krooh@hotmail.com>
2022-05-30 22:10:24 +0300

    publish

---
+++ test/Shapes.test.mjs
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { TShape, IShape, SingleShape } from "../src/Shape.mjs"
+/*
+describe("Rotating shapes", () => {
+    let shape;
+    beforeEach(() => {
+      shape = new TShape('T', 0);
+    });
+  
+    it("initial TShape", () => {
+        expect(shape.toString()).to.equalShape(
+          `.T.
+           TTT
+           ...`
+        );
+    });
+    it("initial TShape", () => {
+        shape.rotateLeft()
+    expect(shape.toString()).to.equalShape(
+        `.T.
+         TT.
+         .T.`
+    );
+    });
+    it("initial TShape", () => {
+        shape.rotateRight()
+    expect(shape.toString()).to.equalShape(
+        `.T.
+         .TT
+         .T.`
+    );
+    });
+});
+*/
