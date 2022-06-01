97d2cac22ab427f2f5e54ac1821bb8c290c7462a
Juho <krooh@hotmail.com>
2022-06-01 10:15:22 +0300

    added game over check

---
+++ test/FullRows.test.mjs
@@ -0,0 +1,41 @@
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+function fallToBottom(board) {
+    for (let i = 0; i < 10; i++) {
+      board.tick();
+    }
+  }
+    
+
+describe("Removing full rows", () => {
+    let board;
+    beforeEach(() => {
+      board = new Board(9, 6);
+    });
+
+    it("start from the top middle", () => {
+        board.drop(Tetromino.T_SHAPE);
+        fallToBottom(board);
+        board.drop(Tetromino.T_SHAPE);
+        board.moveLeft();
+        board.moveLeft();
+        board.moveLeft();
+        fallToBottom(board);
+        board.drop(Tetromino.T_SHAPE);
+        board.moveRight();
+        board.moveRight();
+        board.moveRight();
+        fallToBottom(board);
+        expect(board.toString()).to.equalShape(
+           `.........
+            .........
+            .........
+            .........
+            .........
+            .T..T..T.`
+        );
+    });
+  });
+  
