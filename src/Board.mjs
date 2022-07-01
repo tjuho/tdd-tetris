fb898ae7b918eb1ec316104ec6ef3b14b62a86e5
juho <krooh@hotmail.com>
2022-07-01 10:12:42 +0300

    cleaned code

--- src/Board.mjs
+++ src/Board.mjs
@@ -5,7 +5,6 @@ export class Board {
   width;
   height;
   gameField;
-  shapes;
   fallingShape;
   score;
   level;
@@ -15,7 +14,6 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.shapes = [];
     this.gameField = [];
     for (let r = 0; r < height; r++) {
       this.gameField.push(Array(width))
@@ -66,7 +64,10 @@ export class Board {
   gameOver() {
     console.log('game over');
     this.fallingShape = undefined;
-    this.shapes = [];
+    this.gameField = [];
+    for (let r = 0; r < height; r++) {
+      this.gameField.push(Array(width))
+    }
     this.score.reset();
   }
 
@@ -193,7 +194,6 @@ export class Board {
         this.fallingShape.cornery += 1;
         /*this.signalFallingShapeMoved();*/
       } else {
-        this.shapes.push(this.fallingShape);
         const positions = this.getBlockPositions(this.fallingShape);
         for (let p = 0; p < positions.length; p++) {
           const pos = positions[p];
@@ -222,49 +222,6 @@ export class Board {
     while (this.gameField.length < this.height) {
       this.gameField.unshift(Array(this.width));
     }
-  }
-
-  clearingRows() {
-    let rowIds = this.fullRowIndexes();
-    if (rowIds.length > 0) {
-      this.score.rowsCleared(rowIds.length, this.level);
-      /*this.signalLinesRemoved(rowIds.length);*/
-      for (let j = 0; j < rowIds.length; j++) {
-        let y = rowIds[j];
-        this.removeRow(y);
-      }
-      let foundfallingshape = true;
-      while (foundfallingshape) {
-        for (let i = 0; i < this.shapes.length; i++) {
-          let shape = this.shapes[i];
-          if (this.canFall(shape)) {
-            foundfallingshape = true;
-            shape.cornery += 1;
-          }
-        }
-        foundfallingshape = false;
-      }
-    }
-  }
-
-  fullRowIndexes() {
-    let fullrowindexes = []
-    let str = this.toString();
-    let rows = str.split('\n');
-    for (let r = 0; r < this.height; r++) {
-      let row = rows[r];
-      let isfull = true
-      for (let char of row) {
-        if (char === '.') {
-          isfull = false;
-          break;
-        }
-      }
-      if (isfull) {
-        fullrowindexes.push(r);
-      }
-    }
-    return fullrowindexes;
   }
 
   removeRow(rowIndex) {
@@ -356,37 +313,11 @@ export class Board {
       let pos = positions[i];
       let x = pos[0];
       let y = pos[1];
-      if (!this._isEmpty(x, y, this.shapes)) {
+      if (!this.isEmpty(x, y)) {
         return false;
       }
     }
     return true;
-  }
-
-  _isEmpty(x, y, shapes) {
-    let isempty = true;
-    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
-      return false
-    }
-
-    for (let i = 0; i < shapes.length; i++) {
-      let shape = shapes[i];
-      let positions = this.getBlockPositions(shape);
-      for (let j = 0; j < positions.length; j++) {
-        let pos = positions[j];
-        let tx = parseInt(pos[0]);
-        let ty = parseInt(pos[1]);
-        if (x === tx && y === ty) {
-          isempty = false;
-          break;
-        }
-      }
-    }
-    return isempty;
-  }
-
-  isEmpty(x, y) {
-    return this._isEmpty(x, y, this.shapes);
   }
 
   isEmpty(x, y) {
@@ -475,18 +406,6 @@ export class Board {
     }
   }
 
-
-  getAllShapes() {
-    let shapes = [];
-    for (let i = 0; i < this.shapes.length; i++) {
-      shapes.push(this.shapes[i]);
-    }
-    if (this.fallingShape) {
-      shapes.push(this.fallingShape);
-    }
-    return shapes;
-  }
-
   signalLinesRemoved(count) {
     let obs = this.observers.linesremoved;
     if (!obs) { return }
@@ -553,58 +472,6 @@ export class Board {
       result.push(temp);
     }
     return result;
-  }
-
-  toString() {
-    let str = '';
-    for (let r = 0; r < this.height; r++) {
-      for (let c = 0; c < this.width; c++) {
-        str += '.';
-      }
-      str += '\n';
-    }
-    let shapes = this.getAllShapes();
-    let width = this.width;
-    for (let i = 0; i < shapes.length; i++) {
-      let shape = shapes[i];
-      let positions = this.getBlockPositions(shape);
-      for (let i = 0; i < positions.length; i++) {
-        let pos = positions[i];
-        let x = parseInt(pos[0]);
-        let y = parseInt(pos[1]);
-        let j = x + y * (width + 1);
-        str = str.substring(0, j) + shape.color + str.substring(j + 1);
-      }
-    }
-    return str;
-  }
-
-  toString() {
-    let str = '';
-    for (let r = 0; r < this.height; r++) {
-      for (let c = 0; c < this.width; c++) {
-        str += '.';
-      }
-      str += '\n';
-    }
-    let falling = this.getFallingTetrominoPositionAndColor();
-    let statics = this.getStaticTetrominoPositionsAndColors();
-    if (falling) {
-      statics.push(falling);
-    }
-    for (let i = 0; i < statics.length; i++) {
-      let temp = statics[i];
-      let positions = temp.positions;
-      let color = temp.color;
-      for (let s = 0; s < positions.length; s++) {
-        let pos = positions[s];
-        let x = parseInt(pos[0]);
-        let y = parseInt(pos[1]);
-        let j = x + y * (this.width + 1);
-        str = str.substring(0, j) + color + str.substring(j + 1);
-      }
-    }
-    return str;
   }
 
   toString() {
